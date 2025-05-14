0e3bc436948489e56918279e86c66d12ec06d5de
Ivan Reutenko <reutenkoivan@gmail.com>
2025-05-14 16:06:24 +0300

    commander integration

--- apps/cli-example-1/cli-entry.js
+++ apps/cli-example-1/cli-entry.js
@@ -1,16 +1,31 @@
 #!/usr/bin/env node
 
+const { Command } = require('commander')
 const { shortTask } = require('@cli-template/cli-example-1')
+const packageJson = require('./package.json')
 
-shortTask({
-  taskName: 'Example Task',
-  taskDescription: 'This is an example task.',
-  taskDuration: 1,
-  taskPriority: 'medium',
-})
-  .then(() => {
-    console.log('Task finished!')
+const program = new Command()
+program.name(packageJson.name).description(packageJson.description).version(packageJson.version)
+
+program
+  .command('short-task')
+  .description('Some short task')
+  .argument('<string>', 'Argument to be passed')
+  .option('--boolean-key', 'Boolean option')
+  .option('-t, --template-option <char>', 'Template option', '===')
+  .action((cliArgument, { booleanKey, templateOption }) => {
+    shortTask({
+      taskName: 'short-task',
+      taskDescription: `${JSON.stringify({ booleanKey, templateOption }, null, 2)}`,
+      taskDuration: 1,
+      taskPriority: 'medium',
+    })
+      .then(() => {
+        console.log('Task finished!')
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+      })
   })
-  .catch((error) => {
-    console.error('Error:', error)
-  })
+
+program.parse()
